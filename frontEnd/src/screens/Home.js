957fab795c69296eb0ce43361794630a3d95f5d7
Vincent <daribovincent@gmail.com>
2022-12-05 10:52:40 -0500

    [add] tabs navigator

---
+++ frontEnd/src/screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+
+export default function Home({navigation}) {
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home</Text>
+      <Button title="pres" onPress={() => navigation.navigate('Number')} />
+    </View>
+  );
+}
