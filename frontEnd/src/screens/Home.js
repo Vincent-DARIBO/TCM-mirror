59855056a70477272142985e14979d93149d8ea3
Tom BOULANGER <tom.boulanger@epitech.eu>
2022-12-06 13:41:55 +0100

    init branch tom

--- frontEnd/src/screens/Home.js
+++ frontEnd/src/screens/Home.js
@@ -5,7 +5,7 @@ export default function Home({navigation}) {
   return (
     <View style={{justifyContent: 'center', alignItems: 'center'}}>
       <Text>Home</Text>
-      {/* <Button title="pres" onPress={() => navigation.navigate('Number')} /> */}
+      {<Button title="pres" onPress={() => navigation.navigate('Number')} />}
     </View>
   );
 }
