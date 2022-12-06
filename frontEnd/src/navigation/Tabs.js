1d5a85bcb7f8e8b384b6e4fd67e11ac92afb20e7
Tom BOULANGER <tom.boulanger@epitech.eu>
2022-12-06 14:31:28 +0100

    add screen Number

--- frontEnd/src/navigation/Tabs.js
+++ frontEnd/src/navigation/Tabs.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
+import Number from '../screens/Number'
 
-const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-    </Tab.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Number" component={Number} />
+    </Stack.Navigator>
   );
 }
--- frontEnd/src/screens/Home.js
+++ frontEnd/src/screens/Home.js
@@ -4,8 +4,7 @@ import {Button, Text, View} from 'react-native';
 export default function Home({navigation}) {
   return (
     <View style={{justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home</Text>
-      {<Button title="pres" onPress={() => navigation.navigate('Number')} />}
+      {<Button title="INSCRIPTION" onPress={() => navigation.navigate('Number')} />}
     </View>
   );
 }
