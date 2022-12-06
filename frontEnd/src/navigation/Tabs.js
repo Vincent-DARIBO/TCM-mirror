7a85b3c93271e0cbc5370826ee415c3aa60f0d41
leon <maxime.noel@epitech.eu>
2022-12-06 16:56:07 +0100

    [ADD] chat page

--- frontEnd/src/navigation/Tabs.js
+++ frontEnd/src/navigation/Tabs.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
+import Chat from '../screens/Chat';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +10,7 @@ export default function TabNavigator() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Chat" component={Chat} />
     </Tab.Navigator>
   );
 }
