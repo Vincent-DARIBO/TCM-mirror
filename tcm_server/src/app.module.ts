9e29d9ac017af33d0eaa1e9274392dbb68f5f821
Sebastopaul <sebastien.lamard@epitech.eu>
2022-12-06 15:19:15 +0100

    feat: added connection to database, missing .env, will not push it anyways

--- tcm_server/src/app.module.ts
+++ tcm_server/src/app.module.ts
@@ -4,9 +4,35 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { HobbiesModule } from './hobbies/hobbies.module';
 import { EventsModule } from './events/events.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [UsersModule, EventsModule, HobbiesModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      debug: true,
+      playground: true,
+    }),
+    TypeOrmModule.forRoot({
+      keepConnectionAlive: true,
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    EventsModule,
+    HobbiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- tcm_server/src/hobbies/hobbies.module.ts
+++ tcm_server/src/hobbies/hobbies.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HobbiesService } from './hobbies.service';
 import { HobbiesResolver } from './hobbies.resolver';
+import { Hobby } from './entities/hobby.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Hobby])],
   providers: [HobbiesResolver, HobbiesService],
 })
 export class HobbiesModule {}
