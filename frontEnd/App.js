e5effb962af87661a7e9ad0874be54f3646f909b
Vincent <daribovincent@gmail.com>
2022-12-07 09:10:31 -0500

    [refactor] expo

--- frontEnd/App.js
+++ frontEnd/App.js
@@ -1,17 +1,11 @@
 import 'react-native-gesture-handler';
 
-import 'react-native-gesture-handler';
 import React from 'react';
-import TabNavigator from './src/navigation/Tabs';
-<<<<<<< HEAD
-import {NavigationContainer} from '@react-navigation/native';
-import {Provider} from 'react-native-paper';
-=======
-import Home from './src/screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-native-paper';
->>>>>>> origin/MarcelTest
 
+import Home from './src/screens/Home';
+import TabNavigator from './src/navigation/Tabs';
 export default function App() {
   console.log('app');
   return (
