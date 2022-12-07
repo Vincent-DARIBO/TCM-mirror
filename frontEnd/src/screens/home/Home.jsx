558583c6eb9cc8e58b3b32f649c5034015620c89
Tom BOULANGER <tom.boulanger@epitech.eu>
2022-12-07 12:18:20 +0100

    updates IAM screen

--- frontEnd/src/screens/home/Home.jsx
+++ frontEnd/src/screens/home/Home.jsx
@@ -7,6 +7,7 @@ export default function Home({navigation}) {
       {<Button title="INSCRIPTION" onPress={() => navigation.navigate('Number')} />}
       <Text>voila</Text>
       <Button title="pres" onPress={() => navigation.navigate('Edit profile')} />
+      <Button title="IAM" onPress={() => navigation.navigate('Iam')} />
     </View>
   );
 }
