1c5edc17ab924da22ddb22c5e70b1c4253358888
Tom BOULANGER <tom.boulanger@epitech.eu>
2022-12-07 10:04:59 +0100

    add Code and Iam screen

--- frontEnd/src/navigation/Tabs.js
+++ frontEnd/src/navigation/Tabs.js
@@ -5,6 +5,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import Number from '../screens/Number'
+import Code from '../screens/Code';
+import Iam from '../screens/Iam';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +15,8 @@ export default function TabNavigator() {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Number" component={Number} />
+      <Stack.Screen name="Code" component={Code} />
+      <Stack.Screen name="Iam" component={Iam} />
     </Stack.Navigator>
   );
 }
---
+++ frontEnd/src/screens/Iam.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+
+const Iam = () => {
+    
+    return (
+        <View>
+            <Text>IAM</Text>
+        </View>
+    )
+}
+
+export default Iam
