6ebed0d2af37ca6ee39f438003320f6698d59752
Vincent <daribovincent@gmail.com>
2022-12-12 08:20:52 -0500

    [SWITCH] branch

--- front/src/screens/Events/EventDescription.jsx
+++ front/src/screens/Events/EventDescription.jsx
@@ -1,3 +1,10 @@
 import React from 'react'
+import {View, Image, Button, Icon} from 'react-native'
 
-
+export default function Description() {
+    return <View>
+        <View>
+            <Image source={{}}/>
+        </View>
+    </View>
+}
