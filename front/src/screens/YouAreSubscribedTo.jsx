0bbd4c5e1d8c5b74fe57bdb296b79e4c9145cbd7
marcel4213 <72022917+marcel4213@users.noreply.github.com>
2022-12-12 08:59:01 +0100

    [ADD] add YouAreSubscribedTo

---
+++ front/src/screens/YouAreSubscribedTo.jsx
@@ -0,0 +1,166 @@
+import React from 'react';
+import { FAB } from 'react-native-paper';
+import { Searchbar } from 'react-native-paper';
+import {Ionicons, EvilIcons, Feather} from '@expo/vector-icons';
+import {
+    Keyboard,
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    ScrollView,
+    SafeAreaView,
+    Touchable,
+    TouchableOpacity,
+} from 'react-native';
+import {Button} from 'react-native';
+
+export default function YouAreSubscribedTo() {
+    return (
+        <SafeAreaView style={styles.safeareaview}>
+            <ScrollView>
+                <View style={styles.alignelemView}>
+                    <View>
+                        <Text style={styles.text}>Vous êtes inscrit à:</Text>
+                    </View>
+                    <View>
+                        <Text style={styles.subtext}>Pique-nique avec Marie</Text>
+                    </View>
+                </View>
+                <View style={{marginLeft:5, padding:10}}>
+                    <View style={{flexDirection:'row'}}>
+                        <View style={{paddingHorizontal:5}}>    
+                                <Ionicons 
+                                name="time-outline"
+                                size={25}
+                                onPress={() => console.log('Pressed')}
+                                color="red"
+                                />
+                        </View>
+                        <View>
+                            <Text style={styles.hour}>9:00 AM - 10:00 AM</Text>
+                            <Text style={styles.date}>Mercredi 21 janvier</Text>
+                        </View>
+                    </View>
+                    <View style={styles.sameLigne}>
+                        <Image style={styles.image} source={require("../assets/Femme1.png")} />
+                        <Image style={styles.image} source={require("../assets/marcel.png")} />
+                    </View>
+                </View>
+                <View style={{marginLeft:5, padding:10}}>
+                    <View style={{flexDirection:'row'}}>
+                        <View>
+                            <EvilIcons
+                                name="location"
+                                size={30}
+                                onPress={() => console.log('Pressed')}
+                                color="orange"
+                                />
+                        </View>
+                        <View style={{padding:5}}>
+                            <Text style={styles.place}>TETE D'OR</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={{marginLeft:5, padding:10}}>
+                    <View style={{flexDirection:'row'}}>
+                        <View>
+                            <Feather
+                                name="align-left"
+                                size={30}
+                                onPress={() => console.log('Pressed')}
+                                color="lightblue"
+                                />
+                        </View>
+                        <View style={{flexShrink: 1}}>
+                            <Text style={styles.description}> Bonjour, je m’apelle Marie j’ai 35 ans, traiteuse depuis quelques années maintenant j’aime partager ma cuisine avec du monde. 
+                            C’est pour cela que je vous invite à partager avec moi un jolie pique nique préparer par mes soins. 
+                            Vous pouvez si le souhaiter vous aussi rapporter quelques chose. </Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={{flexDirection:'row', justifyContent:'space-between', padding:10}}>
+                    <TouchableOpacity>
+                        <View>
+                            <View style={styles.buttonRappel}>
+                                <Text style={{color:'white'}}>Créer un rappel</Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.button}>
+                            <Text style={{color:'white'}}>Désinscription</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    safeareaview: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    buttonRappel: {
+        backgroundColor: 'wheat',
+        borderRadius: 8,
+        marginLeft:15,
+        padding: 5,
+        alignItems:'center',
+        width:140
+    },
+    button: {
+        backgroundColor: 'red',
+        borderRadius: 8,
+        marginRight:15,
+        padding: 5,
+        alignItems:'center',
+        width:140
+    },
+    alignelemView: {
+        marginLeft: 10,
+        padding: 20,
+    },
+    place: {
+        color:'darkblue',
+    },
+    description: {
+        color:'darkblue',
+        fontSize:20,
+        textAlign:'justify'
+    },
+    text: {
+        fontSize: 30,
+        color:'darkblue',
+        fontWeight:'bold',
+    },
+    subtext: {
+        fontSize: 30,
+        color:'orange',
+        fontWeight:'bold',
+    },
+    sameLigne:{
+        flexDirection:'row',
+        marginLeft:20
+    },
+    hour:{
+        marginLeft:5, 
+        color:'darkblue', 
+        fontWeight:'bold'
+    },
+    date:{
+        marginLeft:10, 
+        color:'darkblue', 
+        fontWeight:'bold'
+    },
+    image:{
+        width:25,
+        height:25,
+        borderRadius: 10,
+        borderWidth: 1,
+        marginLeft: 5
+      },
+  });
