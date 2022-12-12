fd286f0e55972c357152a2cd962fe1adc793384e
Tania Bezancon <tania.bezancon@epitech.eu>
2022-12-12 10:10:16 +0100

    feat: A branch has been created for CreateAnEvents

--- front/src/screens/Splash/SplashScreen.jsx
+++ front/src/screens/Splash/SplashScreen.jsx
@@ -1,6 +1,5 @@
 import {View, StyleSheet, Image, Text} from 'react-native';
 import React from 'react';
-import Button from '../../components/Button';
 import BackgroundSplash from '../Splash/components/backgroundSplash';
 
 export default function SplashScreen({navigation}) {
@@ -10,11 +9,3 @@ export default function SplashScreen({navigation}) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  background: {
-    marginRight: 300,
-    height: '100%',
-    width: '100%',
-  },
-});
