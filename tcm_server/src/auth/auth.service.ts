8b59a2fb4fadd08d46d4936044a2caf7c3c39a35
Vincent <daribovincent@gmail.com>
2022-12-12 04:30:55 -0500

    [ADD] JWT signup

--- tcm_server/src/auth/auth.service.ts
+++ tcm_server/src/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { compare } from 'bcrypt';
 import { User } from 'src/users/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
+import { Profile } from 'src/profiles/entities/profile.entity';
 
 @Injectable()
 export class AuthService {
@@ -13,6 +18,8 @@ export class AuthService {
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findByMail(email);
+
+    if (!user) throw new NotFoundException(`User ${email} not found`);
     if (await compare(pass, user.profile.password)) {
       const {
         profile: { password, ...rest },
@@ -28,5 +35,19 @@ export class AuthService {
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+  async signUp(profile: Profile) {
+    const user = await this.usersService.findByMail(profile.email);
+    if (user) {
+      console.log(user);
+      throw new UnauthorizedException({
+        statusCode: 1,
+        message: `User ${profile}already exists`,
+      });
+    }
+    const createdUser = await this.usersService.create({
+      profile,
+    });
+    return this.login(createdUser);
   }
 }
