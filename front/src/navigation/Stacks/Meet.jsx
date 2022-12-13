1448d11298f8056b2d1f763c89aeec9ab5770d6c
leon <maxime.noel@epitech.eu>
2022-12-13 11:46:52 +0100

    [ADD] Login screen in tab navigator

--- front/src/navigation/Stacks/Meet.jsx
+++ front/src/navigation/Stacks/Meet.jsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../../screens/home/Home';
+import Login from '../../screens/Login';
 
 const Stack = createNativeStackNavigator();
 
@@ -7,6 +8,7 @@ const MeetStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name='Home' component={Home} options={{}} />
+      <Stack.Screen name='Login' component={Login} options={{}} />
     </Stack.Navigator>
   )
 }
