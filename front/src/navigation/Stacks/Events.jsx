eaac3ff1ac0b2a53865c5de6556dd0539bd3aa9e
Vincent <daribovincent@gmail.com>
2022-12-16 04:11:18 -0500

    [FIX] navigation

--- front/src/navigation/Stacks/Events.jsx
+++ front/src/navigation/Stacks/Events.jsx
@@ -1,22 +1,28 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AddEvent from '../../screens/AddEvent';
 import EventDayDetailsYourActivity from '../../screens/EventDayDetailsYourActivity';
 import YouAreSubscribedTo from '../../screens/YouAreSubscribedTo';
 import EventList from '../../screens/EventList';
 // import EventDescription from '../../screens/Events/EventDescription';
+import CreateAnEventScreen from '../../screens/events/CreateAnEventScreen';
 
 const Stack = createNativeStackNavigator();
 
 const EventsStack = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='EventList' component={EventList} />
-      <Stack.Screen name='AddEvent' component={AddEvent} />
-      <Stack.Screen name='EventDayDetailsYourActivity' component={EventDayDetailsYourActivity} />
-      <Stack.Screen name='YouAreSubscribedTo' component={YouAreSubscribedTo} />
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="EventList" component={EventList} />
+      <Stack.Screen name="AddEvent" component={AddEvent} />
+      <Stack.Screen
+        name="EventDayDetailsYourActivity"
+        component={EventDayDetailsYourActivity}
+      />
+      <Stack.Screen name="YouAreSubscribedTo" component={YouAreSubscribedTo} />
+      <Stack.Screen name="CreateEvent" component={CreateAnEventScreen} />
+
       {/* <Stack.Screen name='EventDescription' component={EventDescription} /> */}
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 export default EventsStack;
--- front/src/screens/Friends.jsx
+++ front/src/screens/Friends.jsx
@@ -1,184 +1,231 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, StyleSheet, Button, Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import React, {useState, useEffect} from 'react';
 import {
-    fadedOrange,
-    orange,
-    primary,
-    secondary,
-    white,
+  FlatList,
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {Searchbar} from 'react-native-paper';
+import {
+  fadedOrange,
+  orange,
+  primary,
+  secondary,
+  white,
 } from '../constants/colors';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-FontAwesome.loadFont()
+FontAwesome.loadFont();
 
 const DATA = [
-    {
-        id: '1',
-        title: 'Lola',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '2',
-        title: 'Emma',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '3',
-        title: 'Gisele',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '4',
-        title: 'Caroline',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '5',
-        title: 'Nadege',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '6',
-        title: 'Adrea',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '7',
-        title: 'Natalie',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '8',
-        title: 'Felice',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '9',
-        title: 'Lea',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '10',
-        title: 'Emilie',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '11',
-        title: 'Juliette',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '12',
-        title: 'Tania',
-        img: require('../assets/Femme1.png'),
-    },
-    {
-        id: '13',
-        title: 'Camille',
-        img: require('../assets/Femme1.png'),
-    },
+  {
+    id: '1',
+    title: 'Lola',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '2',
+    title: 'Emma',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '3',
+    title: 'Gisele',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '4',
+    title: 'Caroline',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '5',
+    title: 'Nadege',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '6',
+    title: 'Adrea',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '7',
+    title: 'Natalie',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '8',
+    title: 'Felice',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '9',
+    title: 'Lea',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '10',
+    title: 'Emilie',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '11',
+    title: 'Juliette',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '12',
+    title: 'Tania',
+    img: require('../assets/Femme1.png'),
+  },
+  {
+    id: '13',
+    title: 'Camille',
+    img: require('../assets/Femme1.png'),
+  },
 ];
 
-const Friends = ({ navigation }) => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const [matchesList, setMatchesList] = useState('');
-    const [loading, setLoading] = useState(false);
+const Friends = ({navigation}) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [matchesList, setMatchesList] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setMatchesList(DATA.filter(item => item.title.includes(searchQuery)))
-        setLoading(false)
-    }, [searchQuery])
+  useEffect(() => {
+    setMatchesList(DATA.filter(item => item.title.includes(searchQuery)));
+    setLoading(false);
+  }, [searchQuery]);
 
-    const render = ({ item, index }) => {
-        return (
-            <TouchableOpacity style={styles.itemCard} onPress={() => navigation.navigate('Profile Details')} >
-                <Image style={{ width: 120, height: 110, marginTop: '2%' }} source={item.img} />
-                <Text style={{
-                    fontSize: 15,
-                    color: primary,
-                    textAlign: 'center',
-                }}>{item.title}</Text>
-                <View style={styles.bottomCardBar} />
-            </TouchableOpacity>
-        )
-    }
-
-    const emptyList = () => {
-        return (
-            <View style={{ alignItems: 'center', flex: 1, flexDirection: 'row', justifyContent: 'center' }} >
-                <Text style={{
-                    paddingTop: '10%',
-                    fontWeight: '500',
-                    fontSize: 15,
-                }}>Ajoutez vos amis pour les voir ici! <FontAwesome name='smile-o' /></Text>
-            </View>
-        )
-    }
-
+  const render = ({item, index}) => {
     return (
-        <View style={{ alignItems: 'center' }}>
-            <View style={{ width: '100%' }} >
-                <Text style={{
-                    paddingTop: 10,
-                    fontWeight: 'bold',
-                    fontSize: 30,
-                    color: primary,
-                    textAlign: 'center'
-                }}>Mes amis</Text>
-                <Searchbar
-                    placeholder='Search'
-                    loading={loading}
-                    onChangeText={query => { setSearchQuery(query); setLoading(true) }}
-                    elevation={4}
-                    style={{ borderRadius: 20, marginLeft: '10%', marginRight: '10%', marginTop: '5%', marginBottom: '5%' }}
-                />
-                <View style={{ flexDirection: 'row-reverse' }} >
-                    <TouchableOpacity onPress={() => navigation.navigate('Requests')}>
-                        <Text style={{
-                            fontWeight: 'bold',
-                            fontSize: 18,
-                            color: secondary,
-                            textAlign: 'right',
-                            paddingRight: 20,
-                            paddingBottom: 10
-                        }} >Demandes (2)</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <View style={{ height: '73%', width: '80%', alignItems: 'center' }}>
-                <FlatList data={matchesList}
-                    renderItem={render}
-                    keyExtractor={item => item.id}
-                    numColumns={2}
-                    ListEmptyComponent={emptyList}
-                    style={{
-                        flex: 1, width: '100%', height: '100%',
-                    }}
-                    showsHorizontalScrollIndicator={false}
-                    showsVerticalScrollIndicator={false}
-                    ItemSeparatorComponent={() => <View style={{ marginVertical: '2%' }} />} />
-            </View>
-        </View>
+      <TouchableOpacity
+        style={styles.itemCard}
+        onPress={() =>
+          navigation.navigate('ProfileDetails', {name: item.title})
+        }>
+        <Image
+          style={{width: 120, height: 110, marginTop: '2%'}}
+          source={item.img}
+        />
+        <Text
+          style={{
+            fontSize: 15,
+            color: primary,
+            textAlign: 'center',
+          }}>
+          {item.title}
+        </Text>
+        <View style={styles.bottomCardBar} />
+      </TouchableOpacity>
     );
-}
+  };
+
+  const emptyList = () => {
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'center',
+        }}>
+        <Text
+          style={{
+            paddingTop: '10%',
+            fontWeight: '500',
+            fontSize: 15,
+          }}>
+          Ajoutez vos amis pour les voir ici! <FontAwesome name="smile-o" />
+        </Text>
+      </View>
+    );
+  };
+
+  return (
+    <View style={{alignItems: 'center'}}>
+      <View style={{width: '100%'}}>
+        <Text
+          style={{
+            paddingTop: 10,
+            fontWeight: 'bold',
+            fontSize: 30,
+            color: primary,
+            textAlign: 'center',
+          }}>
+          Mes amis
+        </Text>
+        <Searchbar
+          placeholder="Search"
+          loading={loading}
+          onChangeText={query => {
+            setSearchQuery(query);
+            setLoading(true);
+          }}
+          elevation={4}
+          style={{
+            borderRadius: 20,
+            marginLeft: '10%',
+            marginRight: '10%',
+            marginTop: '5%',
+            marginBottom: '5%',
+          }}
+        />
+        <View style={{flexDirection: 'row-reverse'}}>
+          <TouchableOpacity onPress={() => navigation.navigate('Requests')}>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 18,
+                color: secondary,
+                textAlign: 'right',
+                paddingRight: 20,
+                paddingBottom: 10,
+              }}>
+              Demandes (2)
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={{height: '73%', width: '80%', alignItems: 'center'}}>
+        <FlatList
+          data={matchesList}
+          renderItem={render}
+          keyExtractor={item => item.id}
+          numColumns={2}
+          ListEmptyComponent={emptyList}
+          style={{
+            flex: 1,
+            width: '100%',
+            height: '100%',
+          }}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={{marginVertical: '2%'}} />}
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    itemCard: {
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        flex: 0.5,
-        aspectRatio: 1,
-        marginHorizontal: '2%',
-        backgroundColor: '#ddd',
-        borderRadius: 5,
-    },
-    bottomCardBar: {
-        width: '100%',
-        backgroundColor: secondary,
-        height: '3%',
-        borderRadius: 5,
-    },
-})
+  itemCard: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flex: 0.5,
+    aspectRatio: 1,
+    marginHorizontal: '2%',
+    backgroundColor: '#ddd',
+    borderRadius: 5,
+  },
+  bottomCardBar: {
+    width: '100%',
+    backgroundColor: secondary,
+    height: '3%',
+    borderRadius: 5,
+  },
+});
 
 export default Friends;
